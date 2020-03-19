8f2ed17c0835a8f62d0b628fd3bdb450b0978b48
JerryRun <565636992@qq.com>
2020-03-19 17:02:02 +0800

    init commit with project base code

---
+++ modules/core/test/com/laurus/kms/KmsTestContainer.java
@@ -0,0 +1,58 @@
+package com.laurus.kms;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class KmsTestContainer extends TestContainer {
+
+    public KmsTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/laurus/kms/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/laurus/kms/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends KmsTestContainer {
+
+        public static final KmsTestContainer.Common INSTANCE = new KmsTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
